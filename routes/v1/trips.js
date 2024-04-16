012d24464809df46a5f06c6bb9660475312e0288
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-16 10:28:25 +0200

    feat(trips): add get and update trips

--- routes/v1/trips.js
+++ routes/v1/trips.js
@@ -29,4 +29,49 @@ router.post("/", async (req, res) => {
   res.json(entry);
 });
 
+// get trips
+
+router.get("/", async (req, res) => {
+  const trips = await prisma.trips.findMany();
+  res.json(trips);
+});
+
+// get snippet with his id
+router.get("/:id", async (req, res) => {
+  const trips = await prisma.trips.findUnique({
+    where: {
+      id: String(req.params.id),
+    },
+  });
+
+  if (!trips) {
+    return next(createError(404, "Trips not found"));
+  }
+
+  res.json(trips);
+});
+
+// update trips
+
+router.patch("/:id", async (req, res) => {
+  let trips;
+
+  try {
+    trips = TripsValidator.parse(req.body);
+  } catch (error) {
+    return res.status(400).json({ errors: error.issues });
+  }
+  const entry = await prisma.trips.update({
+    where: {
+      id: String(req.params.id),
+    },
+    data: {
+      prompt: trips.prompt,
+      output: trips.output,
+    },
+  });
+
+  res.json(entry);
+});
+
 export default router;
