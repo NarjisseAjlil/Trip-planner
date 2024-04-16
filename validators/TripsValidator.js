7e88fb3245a503b9101d62a9f5328b85bb31b1ab
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-16 14:42:40 +0200

    feat(tripvalidator) : add optional  in created and updated

--- validators/TripsValidator.js
+++ validators/TripsValidator.js
@@ -3,8 +3,8 @@ import { z } from "zod";
 const TripsValidator = z.object({
   prompt: z.string(),
   output: z.string(),
-  created_at: z.string().datetime(),
-  updated_at: z.string().datetime(),
+  created_at: z.string().datetime().optional(),
+  updated_at: z.string().datetime().optional(),
 });
 
 export default TripsValidator;
