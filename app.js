7915d7ecd91d27b782b62364e266e1ef1e52e15a
NarjisseAjlil <narjisse.ajlil@gmail.com>
2024-04-19 16:54:01 +0200

    fix(app) : fix port

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ import cors from "cors";
 
 import V1Router from "./routes/v1.js";
 
-const app = express();
+export const app = express();
 
 const URL_FRONT = process.env.URL_FRONT;
-const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,8 +21,4 @@ app.use("/v1", V1Router);
 
 app.use((err, req, res, next) => {
   res.status(err.status).json({ message: err.message });
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
 });
